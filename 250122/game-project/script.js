7c42d026cd8cabf23470413d99466047be409de7
a01040579861 <a01040579861@gmail.com>
2025-01-22 17:30:48 +0900

    250122

---
+++ 250122/game-project/script.js
@@ -0,0 +1,36 @@
+const board = document.querySelector(".board");
+const h1 = document.createElement("h1");
+let itemImg = document.createElement("img");
+let resultImg = document.createElement("img");
+
+h1.innerText = "컴퓨터와 ✌.👊.🖐 맞추기 게임!";
+
+const userChoice = prompt("가위, 바위, 보 중 하나를 선택하세요!", "가위");
+let gameNum;
+
+switch (userChoice) {
+  case "가위":
+    gameNum = 1;
+    break;
+  case "바위":
+    gameNum = 2;
+    break;
+  case "보":
+    gameNum = 3;
+    break;
+  default:
+    alert("잘못선택하셨습니다!!");
+
+    location.reload();
+}
+
+let comChoice = Math.ceil(Math.random() * 3);
+itemImg.src = `./imgs/math_img_${comChoice}.jpg`;
+
+if (gameNum === comChoice) {
+  resultImg.src = `./imgs/game_1.jpg`;
+} else {
+  resultImg.src = `./imgs/game_2.jpg`;
+}
+
+board.append(h1, itemImg, resultImg);
---
+++ 250122/script01.js
@@ -0,0 +1,30 @@
+/* for of문 */
+// const marvels = ["Ironman", "Hulk", "Spiderman", "Thor"];
+
+// for (let marvel of marvels) {
+//   console.log(marvel);
+// }
+
+/* for in 문 */
+// const jsBook = {
+//   title: "재미있는 자바스크립트",
+//   pubDate: "2025-03-01",
+//   pages: 270,
+//   finished: true,
+//   review: function () {
+//     console.log("겁나 재밌군~?");
+//   },
+// };
+
+// for (let key in jsBook) {
+//   console.log(`${key} : ${jsBook[key]}`);
+// }
+
+/* while 문 */
+// let stars = parseInt(prompt("별의 개수:"));
+// while (stars > 0) {
+//   document.write("*");
+//   stars--;
+// }
+
+/* do while 문 */
