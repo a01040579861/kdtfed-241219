cb9037383626daf7d0cfa7c1d66101aa701df01f
a01040579861 <a01040579861@gmail.com>
2025-02-03 18:11:19 +0900

    250203

---
+++ 250203/ts-project/script.ts
@@ -0,0 +1,9 @@
+// type notation = 타입 주석
+// 꼭 타입 주석을 입력하지 않더라도 기본적으로 TS는 타입 추론을 할 수 있음!
+let num: Number = 1;
+let str: String = "Hello";
+let bool: Boolean = true;
+
+// let num: = 1;
+// let str: = "Hello";
+// let bool: = true;
